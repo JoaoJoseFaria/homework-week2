6490b83387b3500ba741c964d56d30df0274bc18
João Faria <joaofaria@MBP-de-Joao.lan>
2015-03-29 21:55:39 +0100

    Exercise 1
    Solve for exercise 1
    Please enter the commit message for your changes. Lines starting

---
+++ exercise1/email2.rb
@@ -0,0 +1,20 @@
+class Email
+	def initialize(subject, header)
+		@subject = subject
+		@header = header
+  end
+  
+  def subject
+    @subject
+  end
+
+  def header
+    @header
+  end
+end
+
+email = Email.new("Keep on coding! :)", { :date => "2014-12-01", :from => "Ferdous" })
+
+puts "Date:    #{email.header[:date]}"
+puts "From:    #{email.header[:from]}"
+puts "Subject: #{email.subject}"
